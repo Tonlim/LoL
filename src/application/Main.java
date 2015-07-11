ab1b61cd63bcbcdfc9bf29db3aa898d965ff594c
Kwinten <kwinten.zaman@hotmail.com>
2015-07-11 14:46:12 +0200

    fuckn met dieje api
    failed :p

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,43 +1,37 @@
 package application;
 	
-import iohandling.APIkey;
-import iohandling.Database;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.GroupLayout.Alignment;
+
 import constant.PlatformId;
+import constant.PlatformId;
+import constant.Region;
 import constant.Region;
 import dto.CurrentGame.CurrentGameInfo;
-import dto.CurrentGame.Participant;
-import dto.Summoner.Summoner;
-import javafx.application.Application;
-import javafx.scene.Scene;
-
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import main.java.riotapi.RiotApi;
-import main.java.riotapi.RiotApiException;
-
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import main.java.riotapi.RiotApi;
-import main.java.riotapi.RiotApiException;
-import constant.PlatformId;
-import constant.Region;
-import data.CustomLeague;
-import data.CustomPlayer;
 import dto.CurrentGame.CurrentGameInfo;
 import dto.CurrentGame.Participant;
 import dto.League.League;
-import dto.Static.Champion;
+import dto.CurrentGame.Participant;
 import dto.Summoner.Summoner;
+import dto.Summoner.Summoner;
+import iohandling.APIkey;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.stage.Stage;
+import main.java.riotapi.RiotApi;
+import main.java.riotapi.RiotApi;
+import main.java.riotapi.RiotApiException;
+import main.java.riotapi.RiotApiException;
 
 
 public class Main extends Application {
@@ -46,8 +40,8 @@ public class Main extends Application {
 		try {
 			RiotApi api = new RiotApi(APIkey.getKey());
 			api.setRegion(Region.EUW);
-			Map<String, Summoner> summoners = api.getSummonersByName("tonlim, elite cart, wolftooth1, zerpee, disney fired me,unrez");
-			Summoner summoner = summoners.get("elitecart");
+			Map<String, Summoner> summoners = api.getSummonersByName("tonlim, elite cart, wolftooth1, zerpee, disney fired me,unrez, colotol");
+			Summoner summoner = summoners.get("colotol");
 			long id = summoner.getId();
 			long level = summoner.getSummonerLevel();
 			
@@ -74,9 +68,9 @@ public class Main extends Application {
 				List<Participant> participants = currentGame.getParticipants();
 				
 				
-				GridPane root = constructRootPane(5,2,participants);
+				GridPane root = constructRootPane(5,2,id);
 				
-				Scene scene = new Scene(root,400,400);
+				Scene scene = new Scene(root,1250,400);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
@@ -164,20 +158,28 @@ public class Main extends Application {
 		launch(args);
 	}
 	
-	private GridPane constructRootPane(int columns,int rows,List<Participant> participants){
+	private GridPane constructRootPane(int columns,int rows,long id) throws RiotApiException{
+		RiotApi api = new RiotApi(APIkey.getKey());
+		CurrentGameInfo currentGame = api.getCurrentGameInfo(PlatformId.EUW, id);
+		List<Participant> participants = currentGame.getParticipants();
+		Map<String, List<League>> mapleague = api.getLeagueEntryBySummoners(id);
 		GridPane result = new GridPane();
 		for(int i = 0; i < columns;i++){
-			result.getColumnConstraints().add(new ColumnConstraints(500));
+			result.getColumnConstraints().add(new ColumnConstraints(250));
 		}
 		//colindex must be indexed with the row number contains the (vertical) grid index where the node should be added
-		int[] colindex= new int[columns];
+		int[] colindex= new int[rows];
 		for(Participant p : participants){
 			int row = (int)(p.getTeamId()/100);
-			int column = colindex[row];
-			colindex[row]++;
+			int column = colindex[row-1];
+			colindex[row-1]++;
 			VBox vb = new VBox();
+			vb.setAlignment(Pos.CENTER);
+			vb.getStyleClass().add("championpanel");
 			Text summonerName= new Text(p.getSummonerName());
 			vb.getChildren().add(summonerName);
+			Summoner summoner = api.getSummonerById(p.getSummonerId());
+			summoner.
 			result.add(vb, column, row);
 		}
 		
