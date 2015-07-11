6ea6eb9390981d9bbf43bfe65ac274f19f903cee
Kwinten <kwinten.zaman@hotmail.com>
2015-07-11 11:25:53 +0200

    Basic visual panes
    Just some normal gui adjustments (displaying info through the gui
    instead of command line)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,22 @@
 package application;
 	
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import main.java.riotapi.RiotApi;
-import main.java.riotapi.RiotApiException;
 import constant.PlatformId;
 import constant.Region;
 import dto.CurrentGame.CurrentGameInfo;
 import dto.CurrentGame.Participant;
-import dto.League.League;
 import dto.Summoner.Summoner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import main.java.riotapi.RiotApi;
+import main.java.riotapi.RiotApiException;
 
 
 public class Main extends Application {
@@ -27,8 +26,8 @@ public class Main extends Application {
 		try {
 			RiotApi api = new RiotApi(key);
 			api.setRegion(Region.EUW);
-			Map<String, Summoner> summoners = api.getSummonersByName("tonlim, elite cart, wolftooth1, zerpee");
-			Summoner summoner = summoners.get("tonlim");
+			Map<String, Summoner> summoners = api.getSummonersByName("tonlim, elite cart, wolftooth1, zerpee, disney fired me,unrez");
+			Summoner summoner = summoners.get("elitecart");
 			long id = summoner.getId();
 			long level = summoner.getSummonerLevel();
 			
@@ -53,21 +52,28 @@ public class Main extends Application {
 			try {
 				CurrentGameInfo currentGame = api.getCurrentGameInfo(PlatformId.EUW, id);
 				List<Participant> participants = currentGame.getParticipants();
-
-				Map<String, Long> teamids = new HashMap<String, Long>();
-				Map<String, Long> champids = new HashMap<String, Long>();
 				
-				long[] idds = new long[10];
+				
+				GridPane root = constructRootPane(5,2,participants);
+				
+				Scene scene = new Scene(root,400,400);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				
+				//test code (old code below)
+				
+				/*long[] idds = new long[10];
 				for(int i=0;i<10;i++){
 					idds[i] = participants.get(i).getSummonerId();
 					teamids.put(""+participants.get(i).getSummonerId(), participants.get(i).getTeamId());
 					champids.put(""+participants.get(i).getSummonerId(), participants.get(i).getChampionId());
 				}
-				
+				*/
 				
 				//TODO: make local database for all champs
 
-				
+				/*
 				Map<String, Summoner> people = api.getSummonersById(idds);
 				ArrayList<String> sumids = new ArrayList<String>(people.keySet());
 
@@ -99,6 +105,7 @@ public class Main extends Application {
 				//RankedStats stats = api.getRankedStats(id);
 				//api.getPlayerStatsSummary(id);
 				
+				
 			} catch(RiotApiException e){
 				System.out.println("No match found");
 				e.printStackTrace();
@@ -108,15 +115,6 @@ public class Main extends Application {
 			//api.getSummonersById();
 			
 			//300px is about 26 chars
-			
-			BorderPane root = new BorderPane();
-			
-
-			
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -124,5 +122,25 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private GridPane constructRootPane(int columns,int rows,List<Participant> participants){
+		GridPane result = new GridPane();
+		for(int i = 0; i < columns;i++){
+			result.getColumnConstraints().add(new ColumnConstraints(500));
+		}
+		//colindex must be indexed with the row number contains the (vertical) grid index where the node should be added
+		int[] colindex= new int[columns];
+		for(Participant p : participants){
+			int row = (int)(p.getTeamId()/100);
+			int column = colindex[row];
+			colindex[row]++;
+			VBox vb = new VBox();
+			Text summonerName= new Text(p.getSummonerName());
+			vb.getChildren().add(summonerName);
+			result.add(vb, column, row);
+		}
+		
+		return result;
 	}
 }
